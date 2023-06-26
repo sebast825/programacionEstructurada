16c69484124c9e93e9d314410a5c675a2b13316d
sebast825 <sebamolina825@gmail.com>
2023-06-26 20:08:35 -0300

    calcular total sanguche

--- SegundoParcial/Program.cs
+++ SegundoParcial/Program.cs
@@ -3,34 +3,96 @@ class Program
 {
    static void Main(string[] args)
    {
-       string MSJ_INGRESO_CANTIDAD_SANGUCHE = "Cuantos sanguches {0} se compraron: ";
+      string MSJ_INGRESO_CANTIDAD_SANGUCHE = "Cuantos sanguches {0} se compraron: ";
       const string MSJ_INGRESO_ERROR_CANTIDAD_SANGUCHE = "La cantidad minima es 1 ";
+      const string MSJE_INGRESO_GUSTO = "Ingrese el nombre del sandwich a consultar: ";
+      const string MSJE_ERROR_INGRESO_GUSTO = "Nombre invalido, intente nuevamente:";
+
       const int MIN_SANGUCHES = 1;
       const int FILAS_MATRIZ = 4;
       const int COLUMNAS_MATRIZ = 3;
-      
+
       int[,] matrizSanguches = new int[FILAS_MATRIZ, COLUMNAS_MATRIZ];
-      string[] sanguchegusto = new string[] { "Jamon-Queso", "Aceituna", "Salame", "Atun" };
-      string[] sangucheVariedad = new string[] { "Simple", "Doble", "Triple" };
+      string[] sanguchegusto = new string[] { "JAMON-QUESO", "ACEITUNA", "SALAME", "ATUN" };
+      string[] sangucheVariedad = new string[] { "SIMPLE", "DOBLE", "TRIPLE" };
+      int[] sangucheTotalGusto = new int[FILAS_MATRIZ];
+      string consultarSanguche;
+      int consultarIndice;
 
 
+
+
+
+      //ingreso de datos
       for (int i = 0; i < matrizSanguches.GetLength(0); i++)
       {
-       Console.Write("Ventas de Sandwiches de ");
-            Console.WriteLine(sanguchegusto[i]);
+         Console.Write("Ventas de Sandwiches de ");
+         Console.WriteLine(sanguchegusto[i]);
          for (int j = 0; j < matrizSanguches.GetLength(1); j++)
          {
-            string mensaje = string.Format(MSJ_INGRESO_CANTIDAD_SANGUCHE,sangucheVariedad[j]);
-            matrizSanguches[i,j] = IngresarInt(mensaje,MSJ_INGRESO_ERROR_CANTIDAD_SANGUCHE,MIN_SANGUCHES);
+            string mensaje = string.Format(MSJ_INGRESO_CANTIDAD_SANGUCHE, sangucheVariedad[j]);
+            matrizSanguches[i, j] = IngresarInt(mensaje, MSJ_INGRESO_ERROR_CANTIDAD_SANGUCHE, MIN_SANGUCHES);
          }
-         MostrarMatriz(matrizSanguches,sanguchegusto,sangucheVariedad);
-
+         MostrarMatriz(matrizSanguches, sanguchegusto, sangucheVariedad);
       }
-MostrarMatriz(matrizSanguches,sanguchegusto,sangucheVariedad);
+
+      //hacer calculos
+      SumarGustoSanguche(matrizSanguches, sangucheTotalGusto);
+
+
+      MostrarMatriz(matrizSanguches, sanguchegusto, sangucheVariedad);
+
+
+      for (int j = 0; j < sangucheTotalGusto.Length; j++)
+      {
+         Console.WriteLine($"{sanguchegusto[j]}: {sangucheTotalGusto[j]}");
+      }
+      //mostrar datos
+
+
+      consultarSanguche = IngresarString(MSJE_INGRESO_GUSTO);
+
+      consultarIndice = IndexArray(consultarSanguche, sanguchegusto);
+      while (consultarIndice == -1)
+      {
+         consultarSanguche = IngresarString(MSJE_ERROR_INGRESO_GUSTO);
+
+         consultarIndice = IndexArray(consultarSanguche, sanguchegusto);
+      }
+
+
+      Console.WriteLine(consultarIndice);
+      Console.WriteLine($"El total de sanguche de {sanguchegusto[consultarIndice]} fue: {sangucheTotalGusto[consultarIndice]}");
 
    }
 
+   static int IndexArray(string valor, string[] arrgelo)
+   {
+      return Array.IndexOf(arrgelo, valor);
+   }
+   static string IngresarString(string mensaje)
+   {
 
+      Console.WriteLine(mensaje);
+      string input = Console.ReadLine();
+      return input.ToUpper();
+   }
+   static void SumarGustoSanguche(int[,] matriz, int[] sangucheTotalGusto)
+   {
+
+      int total = 0;
+      for (int i = 0; i < matriz.GetLength(0); i++)
+      {
+         for (int j = 0; j < matriz.GetLength(1); j++)
+         {
+            total += matriz[i, j];
+
+         }
+         sangucheTotalGusto[i] = total;
+         total = 0;
+      }
+
+   }
    static int IngresarInt(string msje, string msjeError, int min = int.MinValue, int max = int.MaxValue)
    {
 
@@ -45,13 +107,14 @@ MostrarMatriz(matrizSanguches,sanguchegusto,sangucheVariedad);
       return numero;
    }
 
-      static void MostrarMatriz(int[,] matriz, string [] sanguchegusto, string [] sangucheVariedad)
+   static void MostrarMatriz(int[,] matriz, string[] sanguchegusto, string[] sangucheVariedad)
    {
       Console.Write("             ");
-    foreach(string variedad in sangucheVariedad){
-        Console.Write($"{variedad}   ");
+      foreach (string variedad in sangucheVariedad)
+      {
+         Console.Write($"{variedad}   ");
 
-    }
+      }
       Console.WriteLine("        ");
 
       for (int i = 0; i < matriz.GetLength(0); i++)
