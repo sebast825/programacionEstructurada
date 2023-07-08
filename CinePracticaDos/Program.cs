1ec1dca02f2ed27668e6b1d2ff1eea24ccedcbdf
sebast825 <sebamolina825@gmail.com>
2023-07-08 17:00:51 -0300

    metodo automatico

--- CinePracticaDos/Program.cs
+++ CinePracticaDos/Program.cs
@@ -12,8 +12,8 @@ class Program
 
       const string MSJE_INGRESO_MANUAL = "Ingresar la posicion de sala: ";
       const string MSJE_ERROR_INGRESO_MANUAL = "Posicion no valida: ";
-      const int CANT_FILAS = 5;
-      const int CANT_COLUMNAS = 6;
+      const int CANT_FILAS = 2;
+      const int CANT_COLUMNAS = 3;
       const int DNI_MIN = 100;
       const int DNI_MAX = 999;
       const int MAX_ENTRADAS = 6;
@@ -27,7 +27,6 @@ class Program
       int posicionAsientoActual;
       bool cargaExitosa = false;
 
-      Console.WriteLine("Hello, World!");
       //dniIngresado = IngresarInt(MSJE_INGRESO_DNI, MSJE_ERROR_INGRESO_DNI, DNI_MIN, DNI_MAX);
       int num = 45;
       /*
@@ -38,8 +37,17 @@ class Program
             Console.WriteLine(AsientosDisponibles(sala));
           bool asd =  CargarDniManual(sala, NumFila(num), NumColumna(num), 100);
           */
-      cantEntradas = IngresarInt(MSJE_INGRESO_ENTRADAS, MSJE_ERROR_INGRESO_ENTRADAS, MIN_ENTRADAS, MAX_ENTRADAS);
+      
+      while(AsientosDisponibles(sala) > 0){
 
+     
+      cantEntradas = IngresarInt(MSJE_INGRESO_ENTRADAS, MSJE_ERROR_INGRESO_ENTRADAS, MIN_ENTRADAS, MAX_ENTRADAS);
+      while (cantEntradas > AsientosDisponibles(sala))
+      {
+         Console.WriteLine("No hay suficientes entradas disponibles");
+         cantEntradas = IngresarInt(MSJE_INGRESO_ENTRADAS, MSJE_ERROR_INGRESO_ENTRADAS, MIN_ENTRADAS, MAX_ENTRADAS);
+
+      }
       for (int i = 0; i < cantEntradas; i++)
       {
          dniIngresado[i] = IngresarInt(MSJE_INGRESO_DNI, MSJE_ERROR_INGRESO_DNI, DNI_MIN, DNI_MAX);
@@ -66,20 +74,26 @@ class Program
       }
       else if (metodoAsientos == METODO_AUTOMATICO)
       {
+         if(!IsAsientosSeguidos(sala,cantEntradas)){
+            Console.WriteLine("No hay asientos seguidos disponibles, se asignaran donde haya lugar disponible");
+         }
+         AsignarAsientosAutomatico(sala,dniIngresado,cantEntradas);
          Console.WriteLine("automatico");
       }
       else
       {
          Console.WriteLine("error metodo asignacion asientos");
       }
-      /*  foreach (int elem in dniIngresado)
+      
+        foreach (int elem in dniIngresado)
         {
-           Console.WriteLine(elem);
+           Console.Write($"{elem}  ");
         }
-  */
+  
+     // Console.WriteLine(IsAsientosSeguidos(sala,20));
 
       MostrarMatriz(sala);
-
+ }
    }
 
 
@@ -121,6 +135,49 @@ class Program
       return numero;
    }
 
+   static bool AsignarAsientosAutomatico(int[,] matriz, int [] dni, int cantEntradas)
+   {
+      int contador = 0, i = 0, j = 0;
+      while (i < matriz.GetLength(0) && contador < cantEntradas)
+      {
+         j = 0;
+         while (j < matriz.GetLength(1) && contador < cantEntradas)
+         {
+            if (matriz[i, j] == 0)
+            {
+               Console.WriteLine($"posicion: {matriz[i,j]}, contador: {contador}, {dni[contador]}");
+               matriz[i,j] = dni[contador];
+               contador++;
+            }
+          
+            j++;
+         }
+         i++;
+      }
+      return contador == cantEntradas ? true : false;
+   }
+   static bool IsAsientosSeguidos(int[,] matriz, int cantEntradas)
+   {
+      int contador = 0, i = 0, j = 0;
+      while (i < matriz.GetLength(0) && contador < cantEntradas)
+      {
+         j = 0;
+         while (j < matriz.GetLength(1) && contador < cantEntradas)
+         {
+            if (matriz[i, j] == 0)
+            {
+               contador++;
+            }
+            else
+            {
+               contador = 0;
+            }
+            j++;
+         }
+         i++;
+      }
+      return contador == cantEntradas ? true : false;
+   }
    static int AsientosDisponibles(int[,] matriz)
    {
       int contador = 0;
